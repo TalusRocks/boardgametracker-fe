240736217df131ef2fc45b323857ec866c961fb8
RJ <jensenrs@gmail.com>
2018-01-23 19:04:51 -0800

    plays nest sorted by date

--- src/containers/AllPlaysContainer.js
+++ src/containers/AllPlaysContainer.js
@@ -5,17 +5,34 @@ import { fetchPlays } from '../actions'
 import AddPlayButton from '../components/plays/AddPlayButton'
 
 const AllPlaysContainer = ({ allPlays, fetchPlays }) => {
-  // console.log(allPlays, "all plays from AllPlaysContainer")
+
+  const justPlays = []
+  allPlays.byDate.map((el, i) => {
+    justPlays.push(el.plays)
+  })
 
   return (
     <div>
-      <div className="m-1 plays-container">
-        { allPlays.all.map((el, i) => {
-          return <div key={el.$.id} className="mtb-2">
-            <h2 className="mb-025">{el.item[0].$.name}</h2>
-            <p>{el.comments}</p>
-          </div>
+      <div className="plays-container">
+
+        { allPlays.byDate.map((el, i) => {
+          return (
+            <div key={el.date}>
+              <div key={i} className="date-divider">{el.date}</div>
+
+              {el.plays.map((el, j) => {
+                return (
+                  <div className="m-1">
+                    <h2 key={j} className="mb-025">{el.gamename}</h2>
+                    <p>{el.comments ? el.comments : ''}</p>
+                  </div>
+                )
+              })}
+
+            </div>
+          )
         })}
+
       </div>
       <AddPlayButton />
     </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -18,20 +18,53 @@ function allPlays(state = { all: [], byDate: [] }, action) {
   switch (action.type) {
     case PLAYS_LOADED:
 
-      const byDate = action.payload.reduce(function(acc, el){
-        const date = el.$.date
-        acc[date] = acc[date] || []
-        acc[date].push({ name: el.item[0].$.name,
-        comments: el.comments ? el.comments[0] : '' })
-        return acc
-      }, {})
+      // const byDate = action.payload.reduce(function(acc, el){
+      //   const date = el.$.date
+      //   acc[date] = acc[date] || []
+      //   acc[date].push({ name: el.item[0].$.name,
+      //   comments: el.comments ? el.comments[0] : '' })
+      //   return acc
+      // }, {})
 
-      console.log(byDate, "byDate!!!!!!");
+      // 1) group dates
+      const playsByDate = []
+      for (let i = 1; i < action.payload.length; i++) {
+        let prevdate = action.payload[i-1].$.date
+        let date = action.payload[i].$.date
+
+        let dateGroup = { date: '', plays: []}
+        if(date !== prevdate){
+          dateGroup.date = date
+          playsByDate.push(dateGroup)
+        }
+      }
+
+      // 2) add nested game name and comments
+      for (let j = 0; j < action.payload.length; j++) {
+        let date = action.payload[j].$.date
+        let gamename = action.payload[j].item[0].$.name
+        let comments
+        if(action.payload[j].comments){
+          comments = action.payload[j].comments
+        } else {
+          comments = ''
+        }
+
+        for (let k = 0; k < playsByDate.length; k++){
+
+          if(date === playsByDate[k].date){
+            playsByDate[k].plays.push({gamename, comments})
+          }
+        }
+
+      }
+      // console.log(playsByDate, "playsByDate");
+
 
       return {
         ...state,
         all: action.payload,
-        // byDate
+        byDate: playsByDate
       }
     default:
       return state
