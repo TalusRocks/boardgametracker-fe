5c873635b6b774d6bff4d8ebeb4458547079e6f4
RJ <jensenrs@gmail.com>
2018-01-29 10:15:27 -0800

    game name populating to input field on click, and game name and id set to local state - for searched bgg games only

--- src/containers/AddPlayForm.js
+++ src/containers/AddPlayForm.js
@@ -11,25 +11,19 @@ class AddPlayForm extends Component {
     super(props)
 
     this.state = {
-      fireRedirect: false
+      fireRedirect: false,
+      selectedGame: '',
+      selectedGameId: ''
     }
   }
-  //why choose a constructor over this?
-  // state = {
-  //   fireRedirect: false
-  // }
 
-
-  submitNewPlay = (e) => {
-    e.preventDefault()
-
-    const newPlayParams = {
-      playDate: e.target.playDate.value,
-      playGame: e.target.playGame.value,
-      playComments: e.target.playComments.value
-    }
-    // console.log(newPlayParams);
-    // this.setState({ fireRedirect: true })
+  selectBggGame = (e) => {
+    console.log(e.target.id, "e.target.ID!!!");
+    this.setState({selectedGame: e.target.textContent})
+    this.setState({selectedGameId: e.target.id})
+    console.log(this.state.selectedGame, "selectedGame in STATE");
+    console.log(this.state.selectedGameId, "selectedGameId in STATE");
+    document.getElementById('playGame').value = e.target.textContent
   }
 
   sendSearchParam = () => {
@@ -40,8 +34,22 @@ class AddPlayForm extends Component {
     this.props.searchBoardGameGeek(searchGame)
   }
 
+  submitNewPlay = (e) => {
+    e.preventDefault()
+
+    const newPlayParams = {
+      playDate: e.target.playDate.value,
+      playGame: this.state.selectedGame,
+      playGameId: this.state.selectedGameId,
+      playComments: e.target.playComments.value
+    }
+    // console.log(newPlayParams);
+    // REDIRECT OFF FOR DEVELOPMENT
+    // this.setState({ fireRedirect: true })
+  }
+
   render() {
-    console.log(this.props.bggSearchResults.all, "bggSearchResults from redux state");
+    // console.log(this.props.bggSearchResults.all, "bggSearchResults from redux state");
     return this.state.fireRedirect === true ? <Redirect to="/plays"/> : (
       <div>
         <div className="m-1 form-close">
@@ -65,14 +73,17 @@ class AddPlayForm extends Component {
                 <span className="caps-title">
                 Game
                 </span>
-                <input className="mt-05 text-input" type="text" name="playGame" id="playGame" defaultValue="Splendor"></input>
+
+                <input className="mt-05 text-input" type="text" name="playGame" id="playGame" defaultValue="Grand Austria Hotel"></input>
+
               </label>
             </div>
 
             <div onClick={this.sendSearchParam} className="button short-btn">Search BoardGameGeek</div>
 
             <div>{!this.props.bggSearchResults.all ? '' : this.props.bggSearchResults.all.map((el, i) => {
-              return <p key={`${el.name}-${i}`} className="text-center mt-1">{el.name[0].$.value}</p>
+              // console.log(el, "EL");
+              return <p onClick={this.selectBggGame} key={`${el.name}-${i}`} className="blue-link text-center mtb-1" id={el.$.id}>{el.name[0].$.value}</p>
             }) }</div>
 
             <p className="text-center mt-1">Or choose from frequently played:</p>
